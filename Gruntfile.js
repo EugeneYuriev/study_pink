70ea3894d1774e45b1b039eb98d53f0ef1c3e77c
Eugene Yuriev <juriev_gek@mail.ru>
2017-06-23 16:39:16 +0300

    Первоначальная сборка проекта

---
+++ Gruntfile.js
@@ -0,0 +1,169 @@
+"use strict";
+
+module.exports = function(grunt) {
+
+  require("load-grunt-tasks")(grunt);
+
+  grunt.initConfig({
+    less: {
+      style: {
+        files: {
+          "build/css/style.css": "src/less/style.less"
+        }
+      }
+    },
+    postcss: {
+      style: {
+        options: {
+          processors: [
+            require("autoprefixer")({
+              browsers: [
+                "last 1 version",
+                "last 2 Chrome versions",
+                "last 2 Firefox versions",
+                "last 2 Opera versions",
+                "last 2 Edge versions"
+              ]}),
+            require("css-mqpacker")({
+              sort: true
+            })
+          ]
+        },
+        src: "build/css/*.css"
+      }
+    },
+    csso: {
+      style: {
+        options: {
+          report: "gzip"
+        },
+        files: {
+          "build/css/style.min.css": ["build/css/style.css"]
+        }
+      }
+    },
+    uglify: {
+      my_target: {
+        files: {
+          'build/js/scripts.min.js': ['build/js/scripts.js']
+        }
+      }
+    },
+    svgmin: {
+      symbols: {
+        files: [{
+          expand: true,
+          src: ["build/img/*.svg", "build/img/inline/*.svg"]
+        }]
+      }
+    },
+    svgstore: {
+      options: {
+        // cleanup: ['fill', 'style'],
+        svg: {
+          style: "display: none"
+        }
+      },
+      symbols: {
+        files: {
+          "build/img/inline/symbols.svg": ["build/img/inline/*.svg"]
+        }
+      }
+    },
+    imagemin: {
+      images: {
+        options: {
+          optimizationLevel: 3
+        },
+        files: [{
+          expand: true,
+          src: ["build/img/**/*.{png,jpg,gif}"]
+        }]
+      }
+    },
+    browserSync: {
+      server: {
+        bsFiles: {
+          src: [
+            "build/*.html",
+            "build/css/*.css",
+            "build/js/*.js"
+          ]
+        },
+        options: {
+          server: "build/.",
+          watchTask: true,
+          notify: false,
+          open: true,
+          ui: false
+        }
+      }
+    },
+    watch: {
+      html: {
+        files: ["src/*.html"],
+        tasks: ["copy:html"]
+      },
+      js: {
+        files: ["src/js/**"],
+        tasks: ["copy:js", "uglify"]
+      },
+      style: {
+        files: ["src/less/**/*.less"],
+        tasks: ["less", "postcss", "csso"],
+        options: {
+          spawn: false
+        }
+      }
+    },
+    copy: {
+      build: {
+        files: [{
+          expand: true,
+          cwd: "src",
+          src: [
+            "fonts/**/*.{woff,woff2}",
+            "img/**",
+            "js/**",
+            "*.html"
+          ],
+          dest: "build"
+        }]
+      },
+      html: {
+        files: [{
+          expand: true,
+          cwd: "src",
+          src: ["*.html"],
+          dest: "build"
+        }]
+      },
+      js: {
+        files: [{
+          expand: true,
+          cwd: "src",
+          src: ["js/**"],
+          dest: "build"
+        }]
+      }
+    },
+    clean: {
+      build: ["build"]
+    }
+  });
+
+  grunt.registerTask("serve", ["browserSync", "watch"]);
+  grunt.registerTask("symbols", ["svgmin", "svgstore"]);
+
+  grunt.registerTask("build", [
+    "clean",
+    "copy:build",
+    "less",
+    "postcss",
+    "csso",
+    "uglify",
+    "symbols",
+    "imagemin"
+  ]);
+
+};
